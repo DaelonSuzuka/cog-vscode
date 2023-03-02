776e6a8904c1cb7e79bd775be9fdfde0ce6af580
David Kincaid <daelonsuzuka@gmail.com>
2023-03-02 08:07:42 -0500

    Add example C file

---
+++ syntaxes/examples/example.c
@@ -0,0 +1,20 @@
+// This is a Cog block
+/* [[[cog
+    # this is python
+
+    output = 'test output please ignore'
+
+    print(output)
+
+    for c in output:
+        pass
+
+    cog.outl('// ' + output)
+]]] */
+// test output please ignore
+/* [[[end]]] */
+
+// This is C code
+void main(void) {
+    //
+}
